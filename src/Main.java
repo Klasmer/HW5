669c94effdad4b8cd46ba692d23d3e2c476e40cb
klasmer <klasmer495@gmail.com>
2024-08-10 18:56:58 +0300

    Условный оператор. Урок 2 Задача 4. Работа над ошибками

--- src/Main.java
+++ src/Main.java
@@ -37,12 +37,12 @@ public class Main {
         }
 
         System.out.println("Задача 4");
-        short deliveryDistance = 95;
-        if (deliveryDistance <= 20) {
+        short deliveryDistance = 100;
+        if (deliveryDistance < 20) {
             System.out.println("Потребуется дней: " + 1);
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
             System.out.println("Потребуется дней: " + 2);
-        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
             System.out.println("Потребуется дней: " + 3);
         } else {
             System.out.println("Доставки не будет");
