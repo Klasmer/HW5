b3f5d27de8eb1fb3a4ca89b48fc3aa3f34c7807e
klasmer <klasmer495@gmail.com>
2024-08-10 15:19:54 +0300

    Условный оператор. Урок 2 Задача 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" Задача 1");
+        byte clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения Android по ссылке");
+        }
+
+
+
+
     }
 }
