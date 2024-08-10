0f0febb2ed67df1b5c3a123dee0dd9ee3f78ea2c
klasmer <klasmer495@gmail.com>
2024-08-10 17:01:54 +0300

    Условный оператор. Урок 2 Задача 5.

--- src/Main.java
+++ src/Main.java
@@ -42,6 +42,33 @@ public class Main {
         } else {
             System.out.println("Доставки не будет");
         }
+
+        System.out.println("Задача 5");
+        byte monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Сезон зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Сезон весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Сезон лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Сезон осень");
+                break;
+            default:
+                System.out.println("Неверный номер месяца");
+        }
     }
 
 
