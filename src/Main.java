a27efa33da1bc363b9584a240c78418dc4a0708e
klasmer <klasmer495@gmail.com>
2024-08-10 16:02:21 +0300

    Условный оператор. Урок 2 Задача 2. Скорректировал условие выпуска.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Задача 1");
@@ -10,16 +12,19 @@ public class Main {
 
 
         System.out.println("Задача 2");
-        short clientDeviceYear = 2016;
-        if (clientOS == 0 && clientDeviceYear <= 2015) {
+        short clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear > 2015) {
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear <= 2015) {
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите версию приложения Android по ссылке");
         }
+
+
+
 
     }
 }
