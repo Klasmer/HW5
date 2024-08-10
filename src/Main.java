65b635b3d9589a593795cc862f25e9d3c2137117
klasmer <klasmer495@gmail.com>
2024-08-10 15:55:44 +0300

    Условный оператор. Урок 2 Задача 2.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Задача 1");
-        byte clientOS = 1;
+        byte clientOS = 0;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
@@ -9,7 +9,18 @@ public class Main {
         }
 
 
-
+        System.out.println("Задача 2");
+        short clientDeviceYear = 2016;
+        if (clientOS == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения Android по ссылке");
+        }
 
     }
 }
+
